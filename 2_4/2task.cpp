93bb563586a4a259869953f8571efdc49d222e41
Nikerersd <nikrudk0@yandex.ru>
2024-03-31 18:02:34 +0700

    Второе задание

--- 2_4/2task.cpp
+++ 2_4/2task.cpp
@@ -1,29 +1,21 @@
 #include <iostream>
 #include <string>
 #include <set>
-
-bool isPangram(const std::string& sentence) {
-    std::set<char> letters;
+using namespace std;
+bool Pangram(const string& sentence) {
+    set<char> letters;
     for (char ch : sentence) {
-        if (std::isalpha(ch)) {
-            letters.insert(std::tolower(ch));
+        if (isalpha(ch)) {
+            letters.insert(ch);
         }
     }
-
-    return letters.size() == 26; // 26 букв в английском алфавите
+    return letters.size() == 26; //26 letters in the Latin alphabet
 }
 
 int main() {
-    system("chcp 1251");
-    std::string sentence;
-    std::cout << "Введите строку из строчных букв латинского алфавита: ";
-    std::getline(std::cin, sentence);
-
-    if (isPangram(sentence)) {
-        std::cout << "Данная строка является панграммой.\n";
-    } else {
-        std::cout << "Данная строка не является панграммой.\n";
-    }
-
+    string sentence;
+    cout << "Enter a string of lowercase Latin letters: ";
+    getline(cin, sentence);
+    cout << boolalpha << Pangram << endl;
     return 0;
 }
